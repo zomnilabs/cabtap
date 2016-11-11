3e52830617932fb6d296b95df94a177c71af7b03
Alleo Indong <freedom_war550@live.com.ph>
2016-11-12 07:58:38 +0800

    driver maintenance

--- app/src/main/java/com/example/alleoindong/cabtap/admin/AdminActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/admin/AdminActivity.java
@@ -1,15 +1,30 @@
 package com.example.alleoindong.cabtap.admin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
 import com.example.alleoindong.cabtap.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AdminActivity extends AppCompatActivity {
+    @BindView(R.id.btn_driver_maintenance)
+    Button mBtnDriverMaintenance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
         setTitle("Admin");
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_driver_maintenance) void driverMaintenanceClick() {
+        Intent intent = new Intent(this, DriverMaintenanceActivity.class);
+        startActivity(intent);
     }
 }
