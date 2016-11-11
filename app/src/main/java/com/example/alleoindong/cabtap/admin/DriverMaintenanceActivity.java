a51b9237df5a2f71d91f54b6c5d5d4a740767bf6
Alleo Indong <freedom_war550@live.com.ph>
2016-11-12 07:27:02 +0800

    added driver maintenance activity

---
+++ app/src/main/java/com/example/alleoindong/cabtap/admin/DriverMaintenanceActivity.java
@@ -0,0 +1,16 @@
+package com.example.alleoindong.cabtap.admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alleoindong.cabtap.R;
+
+public class DriverMaintenanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_maintenance);
+        setTitle("Driver Maintenance");
+    }
+}
