3e16f0d96b64f573ed00f5063fb35348864e0eda
Alleo Indong <freedom_war550@live.com.ph>
2016-11-12 06:44:23 +0800

    added admin activity

--- app/src/main/java/com/example/alleoindong/cabtap/LoginActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.alleoindong.cabtap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.alleoindong.cabtap.admin.AdminActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.btn_login) Button mLogin;
+    @BindView(R.id.btn_register) Button mRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_login) void loginClick() {
+        Intent intent = new Intent(this, AdminActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/alleoindong/cabtap/admin/AdminActivity.java
@@ -0,0 +1,14 @@
+package com.example.alleoindong.cabtap.admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.alleoindong.cabtap.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
