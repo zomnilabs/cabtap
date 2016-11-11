6b10f91fe96f62dcbe0fab2af7eb43cc54bb2ed4
Alleo Indong <freedom_war550@live.com.ph>
2016-11-12 00:49:31 +0800

    added delay

--- app/src/main/java/com/example/alleoindong/cabtap/SplashScreenActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.alleoindong.cabtap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
