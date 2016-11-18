5f77af9c16c132fb72b5ff3bebac6a049eceaeae
Alleo Indong <freedom_war550@live.com.ph>
2016-11-18 23:51:53 +0800

    click fab button listener

--- app/src/main/java/com/example/alleoindong/cabtap/admin/DriverMaintenanceActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/admin/DriverMaintenanceActivity.java
@@ -1,18 +1,23 @@
 package com.example.alleoindong.cabtap.admin;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.alleoindong.cabtap.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DriverMaintenanceActivity extends AppCompatActivity {
 
-    @BindView(R.id.rcv_drivers) private RecyclerView mRcvDrivers;
+    @BindView(R.id.rcv_drivers) RecyclerView mRcvDrivers;
+    @BindView(R.id.fab_add_driver) FloatingActionButton mFabAddDriver;
+
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView.Adapter mAdapter;
 
@@ -27,5 +32,9 @@ public class DriverMaintenanceActivity extends AppCompatActivity {
 //        mLayoutManager = new LinearLayoutManager(this);
 //        mRcvDrivers.setLayoutManager(mLayoutManager);
 
+    }
+
+    @OnClick(R.id.fab_add_driver) void addDriver() {
+        Toast.makeText(this, "Adding driver", Toast.LENGTH_LONG).show();
     }
 }
