5c1eeb3d55266881a85a453cc015f6ca3b0356db
Alleo Indong <freedom_war550@live.com.ph>
2016-11-18 23:41:10 +0800

    added fab

--- app/src/main/java/com/example/alleoindong/cabtap/admin/DriverMaintenanceActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/admin/DriverMaintenanceActivity.java
@@ -2,15 +2,30 @@ package com.example.alleoindong.cabtap.admin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.alleoindong.cabtap.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DriverMaintenanceActivity extends AppCompatActivity {
 
-    @Override
+    @BindView(R.id.rcv_drivers) private RecyclerView mRcvDrivers;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_maintenance);
         setTitle("Driver Maintenance");
+
+        ButterKnife.bind(this);
+
+        // Recycler view
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mRcvDrivers.setLayoutManager(mLayoutManager);
+
     }
 }
