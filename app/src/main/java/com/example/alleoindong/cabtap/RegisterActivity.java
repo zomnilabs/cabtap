85896ba5e4593d8e3493d37efd539e96076dcf61
Alleo Indong <freedom_war550@live.com.ph>
2016-11-19 14:30:12 +0800

    push

--- app/src/main/java/com/example/alleoindong/cabtap/RegisterActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/RegisterActivity.java
@@ -2,8 +2,16 @@ package com.example.alleoindong.cabtap;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import butterknife.BindView;
 
 public class RegisterActivity extends BaseActivity {
+    @BindView(R.id.first_name) EditText mFirstName;
+    @BindView(R.id.last_name) EditText mLastName;
+    @BindView(R.id.email) EditText mEmail;
+    @BindView(R.id.password) EditText mPassword;
+    @BindView(R.id.confirm_password) EditText mConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
