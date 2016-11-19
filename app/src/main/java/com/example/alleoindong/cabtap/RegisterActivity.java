dd6b1349da5cabab9bfa96461bbfb6adb65d959a
Nina Kristel Cheng <nkmcheng@live.com>
2016-11-19 14:37:08 +0800

    update styles

--- app/src/main/java/com/example/alleoindong/cabtap/RegisterActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/RegisterActivity.java
@@ -17,6 +17,6 @@ public class RegisterActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        setTitle("Register");
+        setTitle("REGISTRATION");
     }
 }
