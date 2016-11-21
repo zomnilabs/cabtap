1e67a75217205569d4891f20ca0e4ee51d6a2f98
Alleo Indong <freedom_war550@live.com.ph>
2016-11-21 23:03:00 +0800

    added add vehicle

--- app/src/main/java/com/example/alleoindong/cabtap/admin/AddVehicleActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/admin/AddVehicleActivity.java
@@ -2,15 +2,63 @@ package com.example.alleoindong.cabtap.admin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.alleoindong.cabtap.R;
+import com.example.alleoindong.cabtap.models.Vehicle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AddVehicleActivity extends AppCompatActivity {
+
+    @BindView(R.id.add_plate) EditText mPlateNumber;
+    @BindView(R.id.add_make) EditText mMake;
+    @BindView(R.id.add_model) EditText mModel;
+    @BindView(R.id.add_year) EditText mYear;
+
+    public DatabaseReference mVehicleRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vehicle);
         setTitle("Add Vehicle");
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_add_vehicle) void addVehicle() {
+        String plateNumber = mPlateNumber.getText().toString();
+        String make = mMake.getText().toString();
+        String model = mModel.getText().toString();
+        String year = mYear.getText().toString();
+
+        Vehicle vehicle = new Vehicle(plateNumber, make, model, year);
+        this.saveVehicle(vehicle);
+    }
+
+    private void saveVehicle(final Vehicle vehicle) {
+        mVehicleRef = FirebaseDatabase.getInstance().getReference("vehicles");
+
+        mVehicleRef.runTransaction(new Transaction.Handler() {
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                mutableData.child(vehicle.plateNumber).setValue(vehicle);
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {
+                finish();
+            }
+        });
     }
 }
