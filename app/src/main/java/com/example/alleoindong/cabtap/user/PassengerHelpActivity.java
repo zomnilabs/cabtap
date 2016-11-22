d58a177900151d1cf3bf670e6f49390d1018a687
Nina Kristel Cheng <nkmcheng@live.com>
2016-11-22 14:35:52 +0800

    added passenger help

---
+++ app/src/main/java/com/example/alleoindong/cabtap/user/PassengerHelpActivity.java
@@ -0,0 +1,17 @@
+package com.example.alleoindong.cabtap.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alleoindong.cabtap.R;
+
+public class PassengerHelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_help);
+
+        setTitle("HELP");
+    }
+}
