0c366130501a016cd38fbf45d97dafb4f32717b6
Alleo Indong <freedom_war550@live.com.ph>
2016-11-25 02:03:33 +0800

    added maintenance model

---
+++ app/src/main/java/com/example/alleoindong/cabtap/models/Maintenance.java
@@ -0,0 +1,26 @@
+package com.example.alleoindong.cabtap.models;
+
+/**
+ * Created by alleoindong on 11/25/16.
+ */
+
+public class Maintenance {
+    public String id;
+    public String vehicle_id;
+    public String maintenance;
+    public String cost;
+    public String maintenanceDate;
+
+    public Maintenance() {
+
+    }
+
+    public Maintenance(String id, String vehicle_id, String maintenance,
+                       String cost, String maintenanceDate) {
+        this.id = id;
+        this.vehicle_id = vehicle_id;
+        this.maintenance = maintenance;
+        this.cost = cost;
+        this.maintenanceDate = maintenanceDate;
+    }
+}
