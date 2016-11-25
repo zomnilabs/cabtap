ff9071d67a187aa03984df53f3c491611c08151f
Nina Kristel Cheng <nkmcheng@live.com>
2016-11-25 17:17:09 +0800

    added how to book

--- app/src/main/java/com/example/alleoindong/cabtap/user/PassengerHelpActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/user/PassengerHelpActivity.java
@@ -1,11 +1,20 @@
 package com.example.alleoindong.cabtap.user;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.alleoindong.cabtap.R;
+import com.example.alleoindong.cabtap.admin.DriverMaintenanceActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PassengerHelpActivity extends AppCompatActivity {
+    @BindView(R.id.how_to_book) TextView mHowToBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,12 @@ public class PassengerHelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_passenger_help);
 
         setTitle("HELP");
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.how_to_book) void howToBookClick() {
+        Intent intent = new Intent(this, HowToBookActivity.class);
+        startActivity(intent);
     }
 }
