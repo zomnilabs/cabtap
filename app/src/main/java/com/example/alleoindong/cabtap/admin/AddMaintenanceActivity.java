c464dbf3d3d91f80ff928faebca911c5532f763f
Alleo Indong <freedom_war550@live.com.ph>
2016-11-26 00:57:45 +0800

    added spinners on add maintenance

--- app/src/main/java/com/example/alleoindong/cabtap/admin/AddMaintenanceActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/admin/AddMaintenanceActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -22,6 +23,8 @@ import butterknife.ButterKnife;
 public class AddMaintenanceActivity extends AppCompatActivity {
 
     @BindView(R.id.maintenance_schedule) EditText mSchedule;
+    @BindView(R.id.maintenance_plate_number) AppCompatSpinner mSpinnerPlateNumber;
+    @BindView(R.id.select_maintenance) AppCompatSpinner mSpinnerMaintenance;
 
     private DialogFragment datePickerDialog;
     private SimpleDateFormat dateFormatter;
