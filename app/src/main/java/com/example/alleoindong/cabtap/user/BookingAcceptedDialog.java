207cbf15e4667c3af90f802ec29f5663016346c5
Alleo Indong <freedom_war550@live.com.ph>
2016-12-01 02:31:39 +0800

    updated booking

---
+++ app/src/main/java/com/example/alleoindong/cabtap/user/BookingAcceptedDialog.java
@@ -0,0 +1,61 @@
+package com.example.alleoindong.cabtap.user;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.alleoindong.cabtap.R;
+
+/**
+ * Created by alleoindong on 12/1/16.
+ */
+
+public class BookingAcceptedDialog extends DialogFragment {
+    public BookingAcceptedDialog() {
+
+    }
+
+    public static BookingAcceptedDialog newInstance(String title, String plateNumber) {
+        BookingAcceptedDialog bookingAcceptedDialog = new BookingAcceptedDialog();
+        Bundle args = new Bundle();
+
+        // Set Arguments
+        args.putString("title", title);
+        args.putString("plateNumber", plateNumber);
+        bookingAcceptedDialog.setArguments(args);
+
+        return bookingAcceptedDialog;
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.booking_request, container);
+
+        String plateNumber = getArguments().getString("plateNumber");
+
+        ((TextView) view.findViewById(R.id.lbl_platenumber)).setText(plateNumber);
+
+        ((Button) view.findViewById(R.id.btn_hide_booking_info)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String title = getArguments().getString("title", "Enter Name");
+        getDialog().setTitle(title);
+    }
+}
--- app/src/main/java/com/example/alleoindong/cabtap/user/RideBookingActivity.java
+++ app/src/main/java/com/example/alleoindong/cabtap/user/RideBookingActivity.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.location.Location;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -216,8 +217,8 @@ public class RideBookingActivity extends AppCompatActivity {
                     return;
                 }
 
+                showBookingRequestDialog(booking);
                 Toast.makeText(RideBookingActivity.this, "A driver has accepted your request", Toast.LENGTH_SHORT).show();
-                finish();
             }
 
             @Override
@@ -227,6 +228,16 @@ public class RideBookingActivity extends AppCompatActivity {
         };
 
         bookingsRef.addValueEventListener(bookingListener);
+    }
+
+    private void showBookingRequestDialog(Booking booking) {
+        FragmentManager fm = getSupportFragmentManager();
+
+        BookingAcceptedDialog bookingAcceptedDialog = BookingAcceptedDialog
+                .newInstance("Passenger Requesting for taxi", booking.plateNumber);
+
+        bookingAcceptedDialog.show(fm, "fragment_booking_accepted");
+
     }
 
     private void getVehicle(String plateNumber) {
